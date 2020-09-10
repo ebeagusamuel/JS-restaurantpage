13f8c6c213f36297731e406698c3a9479038ef59
ebeagusamuel <samuelebeagu@gmail.com>
2020-09-10 15:48:40 +0100

    add function to render pages to the dom

---
+++ src/home.js
@@ -0,0 +1,22 @@
+const home = (() => {
+  let homeDiv = document.createElement('div');
+  homeDiv.classList.add('homepage');
+
+  let homeH2 = document.createElement('h2');
+  homeH2.textContent = 'Welcome to your favourite restaurant.';
+
+  let homeP1 = document.createElement('p');
+  homeP1.textContent = "We know how important delicious food is to our customers, that is why we don not fall short in making sure that we provide the most sumptous and healthy of foods for the satisfaction of our customers."
+
+  let homeP2 = document.createElement('p');
+  homeP2.textContent = "Head over to our menu to see the varieties of awesome dishes we have for you."
+
+  homeDiv.appendChild(homeH2);
+  homeDiv.appendChild(homeP1);
+  homeDiv.appendChild(homeP2);
+
+  return {homeDiv}
+
+})();
+
+export default home
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import './style.css'
 import './images/background-image.jpg'
+import home from './home'
 
+const page = {
+  "Home": home
+};
 const contentDiv = document.getElementById('content');
 const nav = document.createElement('nav')
 nav.classList.add('nav-container');
+
+function clearPage() {
+  contentDiv.lastChild.remove()
+}
+
+function render(val) {
+  clearPage();
+  contentDiv.appendChild(page[val].homeDiv)
+}
 
 ['Home', 'Menu','Contact'].forEach(val => {
   var div = document.createElement('div')
   div.classList.add('nav-tabs');
   div.textContent = val;
-  div.addEventListener('click', () => {
-    console.log(val)
-  })
+  div.addEventListener('click', () => render(val) )
   nav.appendChild(div);
 })
 
 contentDiv.appendChild(nav);
+contentDiv.appendChild(home.homeDiv)
+
