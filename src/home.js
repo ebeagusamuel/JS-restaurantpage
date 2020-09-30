71e483e85bf07f984d227e475d05ebd7a0036a17
ebeagusamuel <samuelebeagu@gmail.com>
2020-09-30 19:49:48 +0100

    update style

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ const home = (() => {
   homeH2.textContent = 'Welcome to your favourite restaurant.';
 
   const homeP1 = document.createElement('p');
-  homeP1.textContent = 'We know how important delicious food is to our customers, that is why we don not fall short in making sure that we provide the most sumptous and healthy of foods for the satisfaction of our customers.';
+  homeP1.textContent = 'We know how important delicious food is to our customers, that is why we do not fall short in making sure that we provide the most sumptous and healthy of foods for the satisfaction of our customers.';
 
   const homeP2 = document.createElement('p');
   homeP2.textContent = 'Head over to our menu to see the varieties of awesome dishes we have for you.';
